d6b586f6c8e9fea69394d87711182b0d1e08a945
Chris Kimball <drooLz@gmail.com>
2018-07-23 01:44:31 +0000

    added template for index.html, updated server.rb touse the new template

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,11 +2,15 @@ package 'httpd' do
   action:install
 end
 
-file '/var/www/html/index.html'do
-  content "<h1>Hello, world!</h1>
-           <h2>IPADDRESS: #{node['ipaddress']}</h2>
-           <h3>HOSTNAME: #{node['hostname']}</h3>"
-  action:create
+##file '/var/www/html/index.html' do
+##  content "<h1>Hello, world!</h1>
+##           <h2>IPADDRESS: #{node['ipaddress']}</h2>
+##           <h3>HOSTNAME: #{node['hostname']}</h3>"
+##  action:create
+##end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'httpd' do
